b92e79bbeb8ae446ec44e1928b5d7dabcefa1c79
rajarajeshvar <rajarajeshvar0203@gmail.com>
2025-12-04 18:12:55 +0530

    Time: 183 ms (5.3%), Space: 16.4 MB (5.13%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        h=""
+        for i in range(len(s)):
+            if (ord(s[i])>=65 and ord(s[i])<=90) or (ord(s[i])>=97 and ord(s[i])<=122) or (ord(s[i])>=48 and ord(s[i])<=57):
+                h+=s[i]
+                
+        h=h.lower()
+        print(h)
+        if h==h[::-1]:
+            
+            return True
+        else:
+            
+            return False      
+        
