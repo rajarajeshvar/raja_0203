227f132244e3a5f1b5fe635df589f4b9b3be5de1
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-02-07 15:18:23 +0530

    Time: 367 ms (54.6%), Space: 12.8 MB (12.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,17 +1,33 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        m=""
-        n=""
+        if not s:
+            return ""
+
+        # Transform string
+        t = "#" + "#".join(s) + "#"
+        n = len(t)
+        p = [0] * n
         
-        for i in range(len(s)):
-            for j in range(i,len(s)):
-                m=m+s[j]
-                if m==m[::-1] and len(m)>len(n):
-                    n=m
-            m=""
-        return n
-        
+        center = right = 0
+
+        for i in range(n):
+            mirror = 2 * center - i
+            print(mirror)
+
+            if i < right:
+                p[i] = min(right - i, p[mirror])
+
+            # Expand around center i
+            while i + p[i] + 1 < n and i - p[i] - 1 >= 0 and \
+                  t[i + p[i] + 1] == t[i - p[i] - 1]:
+                p[i] += 1
+
+            if i + p[i] > right:
+                center = i
+                right = i + p[i]
+
+        max_len = max(p)
+        center_index = p.index(max_len)
+        start = (center_index - max_len) // 2
+
+        return s[start:start + max_len]
