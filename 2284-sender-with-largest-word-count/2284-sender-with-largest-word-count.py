244a15814253cad6eac2be6d2c849f80f08bb40b
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-04 01:29:29 +0530

    Time: 58 ms (75%), Space: 18.2 MB (57.14%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
@@ -8,7 +8,7 @@ class Solution(object):
         d={}
         l=0
         s=""
-        l1=[]
+        
         for i in range(0,len(senders)):
             if senders[i] not in d:
                 d[senders[i]]=len(messages[i].split(" "))
@@ -23,7 +23,5 @@ class Solution(object):
             elif d[i]==l and (i>s):
                 s=i
                 
-        print(l1)
-        print(s)
         return s
         
