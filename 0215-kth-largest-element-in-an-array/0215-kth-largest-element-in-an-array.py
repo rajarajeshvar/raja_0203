e07a5bb3efd0fa4612456536baaed6741c5320d5
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-07 21:06:01 +0530

    Time: 71 ms (89.42%), Space: 19.3 MB (95.55%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums=sorted(nums)
+        k=nums[-k]
+        return k
+        
