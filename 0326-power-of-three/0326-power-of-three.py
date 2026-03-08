5dca72d2eb20e47c5dfff4e49c790d674c03a86e
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-08 19:17:15 +0530

    Time: 14 ms (42.85%), Space: 12.3 MB (84.5%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,14 +1,4 @@
 class Solution(object):
     def isPowerOfThree(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        if (n%3!=0 and n!=1) or n==0:
-            return False
-        for i in range(0,20):
-            if n==3**i:
-                return True
-        return False
-        
+        return n > 0 and (3**19) % n == 0
         
