91764820b392bd828fcc271ea320137b13acee62
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-08 19:15:56 +0530

    Time: 11 ms (60.38%), Space: 12.4 MB (84.5%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -4,8 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        if (n%3!=0 and n!=1) or n==0:
+            return False
         for i in range(0,20):
             if n==3**i:
                 return True
         return False
+        
         
