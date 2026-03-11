b40b8d1e2b0dc6cf5f50edc2621ea992a700324d
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-11 18:58:01 +0530

    Time: 24 ms (82.15%), Space: 13.3 MB (60.9%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -8,8 +8,7 @@ class Solution(object):
         m=1
         nums=sorted(nums,reverse=True)
         m=nums[-1]*nums[-2]*nums[0]
-        for i in range(0,3):
-            n*=nums[i]
+        n=nums[0]*nums[1]*nums[2]
         if n>m:   
             return n
         else:
