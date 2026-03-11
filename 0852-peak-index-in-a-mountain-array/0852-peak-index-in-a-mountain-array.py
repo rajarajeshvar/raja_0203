420740b34056c144e575891ce8a2106d97964312
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-11 19:25:25 +0530

    Time: 8 ms (14.57%), Space: 21.7 MB (63.51%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        return arr.index(max(arr))
+        
