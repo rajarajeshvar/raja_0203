eaadf80d4a39054f8123d300e526612aab6ee542
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-11 19:01:10 +0530

    Time: 33 ms (35.14%), Space: 13.3 MB (60.9%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -4,8 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n=1
-        m=1
+
         nums=sorted(nums)
         m=nums[-1]*nums[-2]*nums[-3]
         n=nums[0]*nums[1]*nums[-1]
