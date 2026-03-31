a8edbf5383957e0f53f6a65e2b76af76bf2a07f1
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-03-31 23:32:08 +0530

    Time: 61 ms (5.19%), Space: 12.4 MB (80.15%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        l=[]
+        s=""
+        m=0
+        
+        for i in paragraph:
+            if i in "!?',;.":
+                l.append(s.lower())
+                s=""
+                continue
+            else:
+                s=s+i
+            if i==" ":
+                s=s[:-1]
+                l.append(s.lower())
+                s=""
+        l.append(s.lower())
+        print(l)
+        n=len(l)
+        d={}
+        for i in range(n):
+            if l[i]=="":
+                continue
+            if l[i] in d:
+                d[l[i]]+=1
+            else:
+                d[l[i]]=1
+        print(d)
+        for i in d:
+            if i not in banned and d[i]>m:
+                m=d[i]
+                s=i
+        return s
+
+        
+
+        
