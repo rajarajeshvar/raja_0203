5aa188e401126723f852d3510c94de661aa4ee9d
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-04-01 23:02:31 +0530

    Time: 10 ms (0.78%), Space: 12.2 MB (93.57%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -5,20 +5,19 @@ class Solution(object):
         :rtype: int
         """
         s=""
-        s1=""
         r=0
         m=0
         while num!=0:
             r=num%2
-            s=str(r)+s
+            if r==0:
+                r="1"
+            if r==1:
+                r="0"
+            s=r+s
             num=num//2
+        print(s)
+        n=len(s)
         for i in s:
-            if i=="0":
-                s1=s1+"1"
-            else:
-                s1=s1+"0"
-        n=len(s1)
-        for i in s1:
             h=int(i)
             n=n-1
             print(h)
