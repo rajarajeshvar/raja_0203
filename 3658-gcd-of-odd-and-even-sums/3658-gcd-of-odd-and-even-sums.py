fb58fa91c540c3869f3af13f8b816d357e94de59
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-07-22 17:44:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (22.05%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n
+        a=1
+        b=2
+        for i in range(n):
+            so+=a
+            se+=b
+            a+=2
+            b+=2
