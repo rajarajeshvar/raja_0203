9638ae72016acc7354c6f636619ba4fc0cbfbdf7
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-07-25 00:38:04 +0530

    Time: 59 ms (92.79%), Space: 19.2 MB (36.74%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def shortestPalindrome(self, s):
+        rev = s[::-1]
+        temp = s + "#" + rev
+
+        lps = [0] * len(temp)
+        j = 0
+
+        for i in range(1, len(temp)):
+            while j > 0 and temp[i] != temp[j]:
+                j = lps[j - 1]
+
+            if temp[i] == temp[j]:
+                j += 1
+                lps[i] = j
+
+        return rev[:len(s) - lps[-1]] + s
