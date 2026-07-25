8675d5ed833e2aed3a4a78896cec6982215e1009
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-07-25 19:58:34 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.08%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -5,15 +5,12 @@ class Solution(object):
         :rtype: int
         """
         l=[]
+        
         m=0
         for i in str(n):
             l.append(int(i))
-
-        
-        for i in range(len(l)):
-            for j in range(i+1,len(l)):
-                if l[i]*l[j]>m:
-                    m=l[i]*l[j]
-                
+        l=sorted(l,reverse=True)
+        m=l[0]*l[1]
+            
         return m
 
