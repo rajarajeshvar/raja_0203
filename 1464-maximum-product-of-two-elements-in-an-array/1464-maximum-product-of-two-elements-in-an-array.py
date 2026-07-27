0998fe240f57be34faf6857e32049375af1070c1
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-07-27 14:07:11 +0530

    Time: 0 ms (100%), Space: 12.4 MB (37.88%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -4,18 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        j=0
-        h1=0
-        h2=0
-        for i in range(len(nums)):
-            if nums[i]>=h1:
-                h1=nums[i]
-                j=i
-                
-        for i in range(len(nums)):
-            if nums[i]<=h1 and i!=j and nums[i]>=h2:
-                h2=nums[i]
-        return (h1-1)*(h2-1)
-
+        nums=sorted(nums,reverse=True)
+        return (nums[0]-1)*(nums[1]-1)
         
         
