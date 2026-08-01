5a1f4606b37054a78bc88b3215bd81eae51e4a02
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-08-01 20:59:51 +0530

    Time: 2908 ms (11.85%), Space: 13.3 MB (57.95%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        for i in range(0,len(nums)):
+            if sum(nums[:i])==sum(nums[i+1:]):
+                return i
+        return -1
+            
+        
