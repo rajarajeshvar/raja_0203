523f88c758213bd1e8575ec7a9fdc477d8305bef
rajarajeshvar <rajarajeshvar0203@gmail.com>
2026-08-09 19:53:57 +0530

    Time: 346 ms (32.29%), Space: 12.6 MB (88.54%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def stoneGameII(self, piles):
+        n = len(piles)
+
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        # dp[i][M] = maximum stones current player can get
+        # starting from index i with M
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            for M in range(1, n + 1):
+
+                # Can take all remaining piles
+                if i + 2 * M >= n:
+                    dp[i][M] = suffix[i]
+                    continue
+
+                # Try taking X piles
+                for X in range(1, 2 * M + 1):
+                    dp[i][M] = max(
+                        dp[i][M],
+                        suffix[i] - dp[i + X][max(M, X)]
+                    )
+
+        return dp[0][1]
